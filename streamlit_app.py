97da7462134c5406900b5e0f1708f8965a688f1d
Yulay77 <t_serayet@etu-webschoolfactory.fr>
2024-05-22 17:34:33 +0200

    plus de sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,45 @@
 import streamlit as st
 import pandas as pd
 import plost
+import streamlit_extras
+from streamlit_extras.add_vertical_space import add_vertical_space
+from streamlit_extras.row import row
 
-st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+st.set_page_config(layout="wide", page_title="Simulateur Engie")
+
+"""## Faire une simulation pour un projet """
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
-st.sidebar.header('Dashboard `version 2`')
-
-st.sidebar.subheader('Heat map parameter')
-time_hist_color = st.sidebar.selectbox('Color by', ('temp_min', 'temp_max')) 
-
-st.sidebar.subheader('Donut chart parameter')
-donut_theta = st.sidebar.selectbox('Select data', ('q2', 'q3'))
-
-st.sidebar.subheader('Line chart parameters')
-plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
-plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
-
-# Row C
+# Row A
 seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
 stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
 
-st.markdown('### Line chart')
+st.subheader('Simulation du projet')
+st.text_input('Nom du projet')
+time_hist_color = st.selectbox('Color by', ('temp_min', 'temp_max')) 
+
+st.markdown('Donut chart parameter')
+donut_theta = st.selectbox('Select data', ('q2', 'q3'))
+
+st.markdown('Line chart parameters')
+plot_data = st.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
+plot_height = st.slider('Specify plot height', 200, 500, 250)
+
+
 st.line_chart(seattle_weather, x = 'date', y = plot_data, height = plot_height)
 
+add_vertical_space(15)
 
-# Row A
+# Row B
 st.markdown('### Metrics')
 col1, col2, col3 = st.columns(3)
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
 
-# Row B
+# Row C
 c1, c2 = st.columns((7,3))
 with c1:
     st.markdown('### Heatmap')
