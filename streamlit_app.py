ed2ffa0438c9f981c7a553ca8597679c1beb3fca
Yulay77 <t_serayet@etu-webschoolfactory.fr>
2024-05-30 11:41:56 +0200

    maj de wording

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,51 +19,51 @@ st.text_input('Nom du projet')
 col1, col2 = st.columns(2)
 
 with col1:
-    nb_designer_senior_prod = selectbox(
-        "Nombre de designer sénior en prod",
+    nb_designer_senior_build_phase = selectbox(
+        "Nombre de designer sénior en build phase",
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
         no_selection_label="---"
     )
 
-    nb_designer_junior_prod = selectbox(
-        "Nombre de designer junior en prod",
+    nb_designer_junior_build_phase = selectbox(
+        "Nombre de designer junior en build phase",
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
         no_selection_label="---"
     )
 
-    nb_dev_senior_prod = selectbox(
-        "Nombre de développeur sénior en prod",
+    nb_dev_senior_build_phase = selectbox(
+        "Nombre de développeur sénior en build phase",
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         no_selection_label="---"
     )
 
-    nb_dev_junior_prod = selectbox(
-        "Nombre de développeur junior en prod",
+    nb_dev_junior_build_phase = selectbox(
+        "Nombre de développeur junior en build phase",
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
         no_selection_label="---"
     )
 
 with col2:
-    nb_designer_senior_maintenance = selectbox(
-        "Nombre de designer sénior en maintenance",
+    nb_designer_senior_run_phase = selectbox(
+        "Nombre de designer sénior en run phase",
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         no_selection_label="---"
    )
 
-    nb_designer_junior_maintenance = selectbox(
-        "Nombre de designer junior en maintenance",
+    nb_designer_junior_run_phase = selectbox(
+        "Nombre de designer junior en run phase",
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
         no_selection_label="---"
     )
 
-    nb_dev_senior_maintenance = selectbox(
-        "Nombre de développeur sénior en maintenance",
+    nb_dev_senior_run_phase = selectbox(
+        "Nombre de développeur sénior en run phase",
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
         no_selection_label="---"
     )
 
-    nb_dev_junior_maintenance = selectbox(
-        "Nombre de développeur junior en maintenance",
+    nb_dev_junior_run_phase = selectbox(
+        "Nombre de développeur junior en run phase",
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         no_selection_label="---"
     )
@@ -73,7 +73,7 @@ tarification = selectbox(
     ["1 à 2 projets", "3 à 7 projets", "8 projets ou plus"],
     no_selection_label="---"
 )
-temps_prod = st.slider('Spécifier la durée du projet en prod', 6, 30, 60)
+temps_build_phase = st.slider('Spécifier la durée du projet en build phase', 6, 60, 24)
 
 if tarification == "1 à 2 projets":
     cout_fluid = 0.17
@@ -84,74 +84,74 @@ elif tarification == "8 projets ou plus":
 else:
     cout_fluid = 0
 
-## Calculs PROD
-designer_prod_senior = nb_designer_senior_prod*int(55000)
-designer_prod_junior = nb_designer_junior_prod*int(40000)
-designer_prod = designer_prod_senior + designer_prod_junior
+## Calculs build_phase
+designer_build_phase_senior = nb_designer_senior_build_phase*int(55000)
+designer_build_phase_junior = nb_designer_junior_build_phase*int(40000)
+designer_build_phase = designer_build_phase_senior + designer_build_phase_junior
 
-dev_prod_senior = nb_dev_senior_prod*int(60000)
-dev_prod_junior = nb_dev_junior_prod*int(42000)
-dev_prod = dev_prod_senior + dev_prod_junior
+dev_build_phase_senior = nb_dev_senior_build_phase*int(60000)
+dev_build_phase_junior = nb_dev_junior_build_phase*int(42000)
+dev_build_phase = dev_build_phase_senior + dev_build_phase_junior
 
-cout_prod_annuel_sf = designer_prod + dev_prod
-cout_prod_mensuel_sf = cout_prod_annuel_sf/12
+cout_build_phase_annuel_sf = designer_build_phase + dev_build_phase
+cout_build_phase_mensuel_sf = cout_build_phase_annuel_sf/12
 
-cout_prod_annuel_af = cout_prod_annuel_sf
-cout_prod_mensuel_af = cout_prod_annuel_af/12
+cout_build_phase_annuel_af = cout_build_phase_annuel_sf
+cout_build_phase_mensuel_af = cout_build_phase_annuel_af/12
 
-## Calculs MAINTENANCE
-designer_maintenance_senior = nb_designer_senior_maintenance*int(55000)
-designer_maintenance_junior = nb_designer_junior_maintenance*int(40000)
-designer_maintenance = designer_maintenance_senior + designer_maintenance_junior
+## Calculs run_phase
+designer_run_phase_senior = nb_designer_senior_run_phase*int(55000)
+designer_run_phase_junior = nb_designer_junior_run_phase*int(40000)
+designer_run_phase = designer_run_phase_senior + designer_run_phase_junior
 
-dev_maintenance_senior = nb_dev_senior_maintenance*int(60000)
-dev_maintenance_junior = nb_dev_junior_maintenance*int(42000)
-dev_maintenance = dev_maintenance_senior + dev_maintenance_junior
+dev_run_phase_senior = nb_dev_senior_run_phase*int(60000)
+dev_run_phase_junior = nb_dev_junior_run_phase*int(42000)
+dev_run_phase = dev_run_phase_senior + dev_run_phase_junior
 
-cout_maintenance_annuel_sf = designer_maintenance + dev_maintenance
-cout_maintenance_mensuel_sf = cout_maintenance_annuel_sf/12
+cout_run_phase_annuel_sf = designer_run_phase + dev_run_phase
+cout_run_phase_mensuel_sf = cout_run_phase_annuel_sf/12
 
-cout_maintenance_annuel_af = cout_maintenance_annuel_sf * 0.66
-cout_maintenance_mensuel_af = cout_maintenance_annuel_af/12
+cout_run_phase_annuel_af = cout_run_phase_annuel_sf * 0.66
+cout_run_phase_mensuel_af = cout_run_phase_annuel_af/12
 
 # COUT FLUID FIXE
-cout_fluid_fix = (cout_maintenance_annuel_sf + cout_prod_annuel_sf) * cout_fluid
+cout_fluid_fix = (cout_run_phase_annuel_sf + cout_build_phase_annuel_sf) * cout_fluid
 
-## Temps de maintenance = 2 ans = 24 mois fixes
-temps_maintenance = 24
+## Temps de run_phase = 2 ans = 24 mois fixes
+temps_run_phase = 24
 
-x_axis = np.arange(0, temps_prod + temps_maintenance)
+x_axis = np.arange(0, temps_build_phase + temps_run_phase)
 
 ## POUR SANS FLUID
 costs_without_fluid = np.zeros(len(x_axis))
 
-# Ajout des coûts mensuel de production sans Fluid
-for i in range(temps_prod):
-    costs_without_fluid[i] = cout_prod_mensuel_sf
+# Ajout des coûts mensuel de build_phaseuction sans Fluid
+for i in range(temps_build_phase):
+    costs_without_fluid[i] = cout_build_phase_mensuel_sf
 
-# Ajout des coûts mensuel de maintenance sans Fluid
-for i in range(temps_prod, len(x_axis)):
-    costs_without_fluid[i] = cout_maintenance_mensuel_sf
+# Ajout des coûts mensuel de run_phase sans Fluid
+for i in range(temps_build_phase, len(x_axis)):
+    costs_without_fluid[i] = cout_run_phase_mensuel_sf
 
 # Calcul des coûts cumulés sans Fluid
 costs_without_fluid = np.cumsum(costs_without_fluid)
 
 ## POUR AVEC FLUID
-temps_prod_af = int(temps_prod * 0.66)
-temps_maintenance_af = 24 + temps_prod * 0.34
+temps_build_phase_af = int(temps_build_phase * 0.66)
+temps_run_phase_af = 24 + temps_build_phase * 0.34
 
 costs_with_fluid = np.zeros(len(x_axis))
 
-# Ajout des coûts mensuel de production avec Fluid
-for i in range(temps_prod_af):
-    costs_with_fluid[i] = cout_prod_mensuel_af
+# Ajout des coûts mensuel de build_phaseuction avec Fluid
+for i in range(temps_build_phase_af):
+    costs_with_fluid[i] = cout_build_phase_mensuel_af
 
-# Ajout des coûts mensuel de maintenance avec Fluid
-for i in range(temps_prod_af, len(x_axis)):
-    costs_with_fluid[i] = cout_maintenance_mensuel_af
+# Ajout des coûts mensuel de run_phase avec Fluid
+for i in range(temps_build_phase_af, len(x_axis)):
+    costs_with_fluid[i] = cout_run_phase_mensuel_af
 
 # Ajustement du premier mois Avec cout fluid fixe
-costs_with_fluid[0] = cout_prod_mensuel_af + cout_fluid_fix
+costs_with_fluid[0] = cout_build_phase_mensuel_af + cout_fluid_fix
 
 # Calcul des coûts cumulés avec Fluid
 costs_with_fluid = np.cumsum(costs_with_fluid)
@@ -162,6 +162,8 @@ data = {'Avec Fluid': costs_with_fluid,
 df = pd.DataFrame(data, index=x_axis)
 
 st.line_chart(data, use_container_width=True, y=['Avec Fluid', 'Sans Fluid'])
+
+st.write(df.round(0))
 
 
 
